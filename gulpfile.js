ca2b1b88051c0ef3935aa4cfa8590bc78367e078
ukatama <dev.ukatama@gmail.com>
2015-05-26 12:12:46 +0900

    Add watcher and sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 var gulp = require('gulp');
+var plumber = require('gulp-plumber');
 var react = require('gulp-react');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('jsx', function () {
     return gulp.src('jsx/**/*.jsx')
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
         .pipe(react())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('js'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('jsx/**/*.jsx', ['jsx']);
 });
 
 gulp.task('default', ['jsx']);
--- js/hello.js
+++ js/hello.js
@@ -2,3 +2,5 @@ React.render(
     React.createElement("h1", null, "Hello, world!"),
     document.getElementById('example')
 );
+
+//# sourceMappingURL=hello.js.map
